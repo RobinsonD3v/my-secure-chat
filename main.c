772831b57af7c8180953bdcb1e41e9954b07b44f
Robinson <70570204+RobinsonD3v@users.noreply.github.com>
2023-01-06 19:42:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
-#include "chat.h"
-//c'est ici que tout vas se lancer
+int main()
+{
+    pthread_t threads[2];
+    if(pthread_create(&threads [0],NULL,client_process,NULL) != 0){
+	    		printf("client thread failled\n");
+	    	}
+	if(pthread_create(&threads [1],NULL,serveur_process,NULL) != 0){
+	    		printf("serveur thread failled\n");
+	    	}
+	pthread_join (threads[0], NULL);
+	pthread_cancel(threads[1]);
 
-int main(){
-	printf("bienvenue sur mon chat !!!\n\n");
-	serveur();
-	//client();
-	return 0;
+    return 0;
 }
