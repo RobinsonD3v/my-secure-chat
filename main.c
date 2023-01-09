be5ae69d0d722e5c1db26d91900c3dcc89a345bd
Robinson <70570204+RobinsonD3v@users.noreply.github.com>
2023-01-09 17:01:55 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,15 +3,27 @@
 
 int main()
 {
+	printf("\n");
+	printf("                    /\\_/\\           ___\n");
+	printf(" My Secure Chat    = o o =_______    \\ \\\n");
+	printf("  by Robinson       \\_-     __(  \\.__) )\n");
+	printf("                   <_____>__(_____)____/\n");
+
     pthread_t threads[2];
     if(pthread_create(&threads [0],NULL,client_process,NULL) != 0){
-	    		printf("client thread failled\n");
+	    		print_log("client : thread failled\n");
+	    		printf("une erreur est survenue\n");
 	    	}
 	if(pthread_create(&threads [1],NULL,serveur_process,NULL) != 0){
-	    		printf("serveur thread failled\n");
+	    		print_log("serveur : thread failled\n");
+	    		printf("une erreur est survenue\n");
 	    	}
+
 	pthread_join (threads[0], NULL);
 	pthread_cancel(threads[1]);
-
+	print_log("serveur: stop\n");
+	
     return 0;
 }
+
+//le makefile et threadmax
